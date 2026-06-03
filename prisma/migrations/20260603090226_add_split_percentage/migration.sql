cd42ec897a2d7187fdcfdf6d722f6cbf74b585a3
FyslAhmd <info.faysal.32@gmail.com>
2026-06-03 15:02:53 +0600

    feat: add splitPercentage column to salon_entries and split_entries tables

---
+++ prisma/migrations/20260603090226_add_split_percentage/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "salon_entries" ADD COLUMN     "splitPercentage" DOUBLE PRECISION DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "split_entries" ADD COLUMN     "splitPercentage" DOUBLE PRECISION DEFAULT 0;
